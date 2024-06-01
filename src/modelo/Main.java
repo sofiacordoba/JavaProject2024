008892f2a28d70dc2ae83d292b319fd763d158df
josefinagarriz <131292319+josefinagarriz@users.noreply.github.com>
2024-05-31 23:27:11 -0300

    Update Main.java
    cambios en cancelar reserva aula

--- src/modelo/Main.java
+++ src/modelo/Main.java
@@ -14,20 +14,21 @@ public class Main {
 
     public static void cancelarReservaAula(List<Aula> aulas, int idAula, int codRes) //cancela una reserva en un aula
     {
-        Aula aulaCancelar = null;
-        for(Aula aulaAct : aulas) //busca el aula con la reserva a cancelar
-        {
-            if (aulaAct.getID() == idAula)
-            {
-                aulaCancelar= aulaAct;
-                break;
-            }
-        }
+       Aula aulaCancelar = null; //auxiliar que guarda aula con reserva para borrar
+        Iterator<Aula>iterador = aulas.iterator(); //auxiliar para recorrer lista, es un iterador,no un contenedor
+        Aula aulaAct; //el aula actual, se usa en el while
 
+        while (aulaCancelar == null && iterador.hasNext()) //mientras que el actual tenga siguiente y todavía no se encontró el aula, sigue buscando
+        {
+            aulaAct=iterador.next(); //va ciclando
+            if (aulaAct.getID() == idAula) //si lo encuentra, lo asigna para cancelar
+                aulaCancelar = aulaAct;
+        }
+        
         if (aulaCancelar != null) //encontró el aula con la reserva a cancelar
             aulaCancelar.cancelarReserva(codRes);
         else //no encontró el aula
-            System.out.println("No se encontró el aula "+ idAula);
+            System.out.println("No se encontró el aula "+ idAula+ ".");
 
     }
 
