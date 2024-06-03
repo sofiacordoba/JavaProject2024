8b20ef5895c47b1731376482ab81c70dd02dbf2d
josefinagarriz <131292319+josefinagarriz@users.noreply.github.com>
2024-06-02 22:01:53 -0300

    Update Main.java
    mejor eficiencia, ahora busca ordenadamente

--- src/modelo/Main.java
+++ src/modelo/Main.java
@@ -14,19 +14,16 @@ public class Main {
 
     public static void cancelarReservaAula(List<Aula> aulas, int idAula, int codRes) //cancela una reserva en un aula
     {
-       Aula aulaCancelar = null; //auxiliar que guarda aula con reserva para borrar
-        Iterator<Aula>iterador = aulas.iterator(); //auxiliar para recorrer lista, es un iterador,no un contenedor
-        Aula aulaAct; //el aula actual, se usa en el while
+       Iterator<Aula>iterador = aulas.iterator(); //auxiliar para recorrer lista, es un iterador,no un contenedor
+        Aula aulaAct=iterador.next(); //el aula actual, se usa en el while
 
-        while (aulaCancelar == null && iterador.hasNext()) //mientras que el actual tenga siguiente y todavía no se encontró el aula, sigue buscando
+        while (iterador.hasNext() && aulaAct.getID() < idAula) //mientras que el actual tenga siguiente y el ID del aula actual sea menor al ID del aula que buscamos
         {
             aulaAct=iterador.next(); //va ciclando
-            if (aulaAct.getID() == idAula) //si lo encuentra, lo asigna para cancelar
-                aulaCancelar = aulaAct;
         }
-        
-        if (aulaCancelar != null) //encontró el aula con la reserva a cancelar
-            aulaCancelar.cancelarReserva(codRes);
+
+        if (aulaAct != null && aulaAct.getID() == idAula) //si lo encuentra, lo cancela
+            aulaAct.cancelarReserva(codRes);
         else //no encontró el aula
             System.out.println("No se encontró el aula "+ idAula+ ".");
 
