2cbb552ee498aecb72f6f11237ae86d147bd62ca
josefinagarriz <131292319+josefinagarriz@users.noreply.github.com>
2024-06-10 00:49:38 -0300

    Update Aula.java

--- src/Modelo/Aula.java
+++ src/Modelo/Aula.java
@@ -1,6 +1,9 @@
 package modelo;
 
+import Excepciones.ExcepcionCodNoEncontrado;
 import java.util.*;
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 /** Este es mi primer programa
  * Simplemente escribe la frase "iHola, mundo!"
@@ -27,40 +30,7 @@ public class Aula {
     public int getID(){ return ID; }
     public int getCapacidad(){ return capacidad; }
 
-    public boolean disponible (Calendar fechaHoraNuevo, int duracionNuevo) //ve si el aula está disponible en determinado día y horario
-    {
-        Calendar finNuevaReserva = (Calendar) fechaHoraNuevo.clone(); // Calcula el tiempo de finalización de la nueva reserva
-        finNuevaReserva.add(Calendar.MINUTE, duracionNuevo);
-
-        Iterator<Reserva> iterador = reservas.iterator(); //auxiliar para recorrer lista, es un iterador,no un contenedor
-        boolean disponible= true; //aumimos que el aula está disponible
-        Reserva reservaAct; //la reserva actual, se usa en el while
-
-        while (iterador.hasNext() && disponible)
-        {
-            reservaAct=iterador.next();
-            Calendar finReserva = (Calendar) reservaAct.getFechaHora().clone(); // Calcula el tiempo de finalización de la reserva actual
-            finReserva.add(Calendar.MINUTE, reservaAct.getDuracion());
-
-            // Verifica si la nueva reserva se solapa con la reserva actual
-            if (reservaAct.getFechaHora().before(finNuevaReserva) && fechaHoraNuevo.before(finReserva)) // Si hay solapamiento, el aula no está disponible
-                disponible = false;
-        }
-
-        return disponible;
-    }
-
-    public boolean capaz (int c) //ve si el aula es capaz de tener determinado numero de personas
-    {
-        return c <= capacidad;
-    }
-
-    public void agregarReserva(Reserva reserva) //agrega reserva, no ordenado
-    {
-        reservas.add(reserva);
-    }
-
-    public void cancelarReserva(int codRes) //cancela una reserva con su código dado
+    public void cancelarReserva(int codRes) throws ExcepcionCodNoEncontrado//cancela una reserva con su código dado
     {
         Reserva reservaCancelar = null;    //auxiliar que guarda reserva para borrar
         Iterator<Reserva> iterador = reservas.iterator();  //auxiliar para recorrer lista, es un iterador,no un contenedor
@@ -75,27 +45,39 @@ public class Aula {
             }
         }
 
-        try {
-            if (reservaCancelar != null) {    // Si reservaCancelar no es nulo, se elimina la reserva de la lista reservas
-                reservas.remove(reservaCancelar);
-                System.out.println("Se canceló la reserva " + codRes + " exitosamente.");
-            } else {         // Si reservaCancelar es nulo, se lanza una nueva excepción
-                throw new Exception("No se encontró la reserva " + codRes + ".");
+        if (reservaCancelar != null) // Si reservaCancelar no es nulo, se elimina la reserva de la lista reservas
+        {
+            reservas.remove(reservaCancelar);
+            System.out.println("Se canceló la reserva " + codRes + " exitosamente.");
+        }
+        else
+            throw new ExcepcionCodNoEncontrado("No se enconró el código de la reserva " + codRes+".");
+    }
+
+    public boolean capaz (int c) //ve si el aula es capaz de tener determinado numero de personas
+    {
+        return c <= capacidad;
+    }
+
+    public boolean disponible (LocalDate fechaNueva, LocalTime horaInicioN, LocalTime horaFinN) //ve si el aula está disponible en determinado día y horario
+    {
+        Iterator<Reserva> iterador = reservas.iterator(); //auxiliar para recorrer lista, es un iterador,no un contenedor
+        boolean disponible= true; //aumimos que el aula está disponible
+        Reserva reservaAct; //la reserva actual, se usa en el while
+
+        while (iterador.hasNext() && disponible)
+        {
+            reservaAct=iterador.next(); //cicla
+
+            if (fechaNueva.isEqual(reservaAct.getFecha()))
+            {
+                // Verifica si la nueva reserva se solapa con la reserva actual
+                if (reservaAct.getHoraInicio().isBefore(horaFinN) && reservaAct.getHoraFin().isAfter(horaInicioN)) // Si hay solapamiento, el aula no está disponible
+                    disponible = false;
             }
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
         }
 
-/*
-        if (reservaCancelar != null) {
-            try {
-                reservas.remove(reservaCancelar);
-            } catch (Excepcion e) {
-                System.out.println("Error al cancelar reserva: " + e.getMessage());
-            }
-        } else {
-            System.out.println("No se encontró la reserva " + codRes +".");
-        }*/
+        return disponible;
     }
 
     @Override
