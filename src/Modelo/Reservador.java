ea2d180d05e96243a59164e67960ed7279fda605
josefinagarriz <131292319+josefinagarriz@users.noreply.github.com>
2024-06-10 00:58:56 -0300

    Update Reservador.java

--- src/Modelo/Reservador.java
+++ src/Modelo/Reservador.java
@@ -4,18 +4,20 @@ public abstract class Reservador {
     //Atributos:
     private String codigo;
     private String descripcion;
+    private int cantParticipantes;
 
     //Métodos:
 
     //Constructor
-    public Reservador (String cod, String desc)
+    public Reservador (String cod, String desc, int cantpart)
     {
         codigo=cod;
         descripcion=desc;
+        cantParticipantes=cantpart;
     }
 
     //getters
     public String getCodigo() { return codigo; }
     public String getDescripcion(){ return descripcion; }
-
+    public int getCantParticipantes(){ return cantParticipantes; }
 }
